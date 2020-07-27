ece831dac1bf8692c161281b478e67f9b47ad2c8
Tanbos <tbosonchenko@gmail.com>
2020-07-27 13:20:22 +0300

    format..., control useKey, style

--- js/keys.js
+++ js/keys.js
@@ -20,12 +20,7 @@ Vue.component('keys', {
   },
   template: `
   <div>
-<!--  <span>Зібрано ключів: {{foundkeys}} </span>-->
     <img  :src="urlKey[numberKeys]" height="100px" width="200px"> 
   </div>
-<!--  <img -->
-<!--    v-for="i in 3" -->
-<!--    :src="foundkeys === i ? urlKey[i] : urlKey[0]">-->
-<!--  </div>-->
   `
 })
